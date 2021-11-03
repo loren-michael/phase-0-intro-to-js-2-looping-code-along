320f0fc4804569ea0a50c3f725f25b940e874110
loren <lorenmichaelh@gmail.com>
2021-11-03 16:22:45 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
